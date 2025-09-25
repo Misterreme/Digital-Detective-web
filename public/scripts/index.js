21e8663ef3360331d8c47694011037aa0691ba56
Misterreme <nunezemmanuel107@gmail.com>
2025-09-25 15:00:09 -0400

    Errores corregidos

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,42 +1,42 @@
 const $ = (id) => document.getElementById(id);
-const navBar = $('nav-bar');
-const submitButton = $('submit-button');
 
-submitButton.addEventListener("submit", async (e) => {
-      e.preventDefault();
+const navBar = $("nav-bar");
+const form = document.querySelector("form");
 
-      const formData = {
-        name: e.target.name.value,
-        email: e.target.email.value,
-        message: e.target.message.value,
-      };
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-      try {
-        const resp = await fetch("/api/send", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(formData),
-        });
+  const formData = {
+    name: $("#name-input").value,
+    email: $("#email-input").value,
+    message: $("#textarea-message").value
+  };
 
-        const data = await resp.json();
-        if (data.ok) {
-          alert("✅ Mensaje enviado correctamente");
-          e.target.reset();
-        } else {
-          alert("❌ Error: " + data.error);
-        }
-      } catch (err) {
-        alert("❌ Error al conectar con el servidor");
-      }
+  try {
+    const resp = await fetch("/api/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData)
     });
 
-window.addEventListener("scroll", (e) => {
-    if (window.scrollY > 10) {
-        navBar.classList.add("backdrop-blur-lg");
-        navBar.classList.remove("bg-white");
-
+    const data = await resp.json();
+    if (data.ok) {
+      alert("✅ Mensaje enviado correctamente");
+      form.reset();
     } else {
-        navBar.classList.remove("bg-red-500");
-        navBar.classList.add("bg-white");
-    };
+      alert("❌ Error: " + data.error);
+    }
+  } catch (err) {
+    alert("❌ Error al conectar con el servidor");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    navBar.classList.add("backdrop-blur-lg");
+    navBar.classList.remove("bg-white");
+  } else {
+    navBar.classList.remove("bg-red-500");
+    navBar.classList.add("bg-white");
+  }
 });
