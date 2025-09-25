a5cefedd30d916c000cea4610f9a30b0a43a8f23
Misterreme <nunezemmanuel107@gmail.com>
2025-09-25 14:23:21 -0400

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+const $ = (id) => document.getElementById(id);
+const navBar = $('nav-bar')
+
+window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 10) {
+        navBar.classList.add("backdrop-blur-lg");
+        navBar.classList.remove("bg-white");
+
+    } else {
+        navBar.classList.remove("bg-red-500");
+        navBar.classList.add("bg-white");
+    };
+});
