a720896a7964824e2681e3137a15188c7a7b230c
Misterreme <nunezemmanuel107@gmail.com>
2025-09-25 15:48:03 -0400

    Dejar de rastrear archivos ignorados

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,39 +1,6 @@
 const $ = (id) => document.getElementById(id);
 
 const navBar = $("nav-bar");
-const form = document.querySelector("form");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const nameInput = $('name-input');
-  const emailInput = $('email-input');
-  const messageTextArea = $('textarea-message');
-
-  const formData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageTextArea.value
-  };
-
-  try {
-    const resp = await fetch("/api/send", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData)
-    });
-
-    const data = await resp.json();
-    if (data.ok) {
-      alert("✅ Mensaje enviado correctamente");
-      form.reset();
-    } else {
-      alert("❌ Error: " + data.error);
-    }
-  } catch (err) {
-    alert("❌ Error al conectar con el servidor");
-  }
-});
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 10) {
